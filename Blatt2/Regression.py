f62d8e4f530bc5951b11a3c367d03af95d161c18
Andre Immig <aimmig@students.uni-mainz.de>
2017-05-04 21:58:59 +0200

    Added first version of linear Regression

--- Blatt2/Regression.py
+++ Blatt2/Regression.py
@@ -9,8 +9,23 @@ def loadData(fileName):
 def getDataSegment(data,start,end,colum):
         return data[start:end:,colum]
 
+#calulates m,b for m*x+b for regression on x and y
+def linearRegression(x,y):
+        n=len(x)
+        average_x=np.sum(x)/n
+        average_y=np.sum(y)/n
+        scalar_term=np.inner(x,y)
+        squared_sum=np.sum(x**2)
+        m=(n*average_x*average_y-scalar_term)/(n*average_x*average_x-squared_sum)
+        return (m,average_y-m*average_x)
+
+#evaluates m*x+b at all positions x
+def linearFunction(m,b,x):
+        return m*x+b
+
 #plots the graphs
 def plotGraphs(temperature,rain,start,end):
+	#----Aufgabe 1.1 Darstellung des Datensatzes-------
         linestyle="-"
         temperature_color="red"
         rain_color="blue"
@@ -22,39 +37,31 @@ def plotGraphs(temperature,rain,start,end):
         ax1.plot(x,temperature,linestyle,color=temperature_color)
         ax1.tick_params('y',colors=temperature_color)
         ax1.set_xlabel("Zeitpunkt")
+        ax1.set_ylabel("Temperatur")
         #ax2 with same x-axes for rain
         ax2=ax1.twinx()
         ax2.tick_params('y',colors=rain_color)
         ax2.plot(x,rain,"-",color=rain_color)
-        plt.show()
-        #----------------
-        #select every selections.th point from the data
-        selection=10
-        #selected x_values
-        x_selected=np.arange(start,end,selection)
+        ax2.set_ylabel("Niederschlag")
+        #----Aufgabe 1.4 lineareRegression (erster Teil)------
+        (m_temp,b_temp)=linearRegression(x,temperature)
+        (m_rain,b_rain)=linearRegression(x,rain)
+        boundary_points=np.array([start,end])
+        ax1.plot(boundary_points,linearFunction(m_temp,b_temp,boundary_points),":",color="firebrick")
+        ax2.plot(boundary_points,linearFunction(m_rain,b_rain,boundary_points),":",color="royalblue")
+        #----Aufgabe 1.4 (letzter Teil) für allgemeine Polynome-----
         #degree of interpolated polynomial
         degree=5
-        #interpolate polynomial with selected data
-        temperature_selected=temperature[x_selected-start]
-        coeff_temperature=np.polyfit(x_selected,temperature_selected,degree)
+        #interpolate polynomial with data
+        coeff_temperature=np.polyfit(x,temperature,degree)
+        coeff_rain=np.polyfit(x,rain,degree)
         p_temperature=np.poly1d(coeff_temperature)
+        p_rain=np.poly1d(coeff_rain)
         #calculate polynomial at all positions
         values_temperature=p_temperature(x)
-        rain_selected=rain[x_selected-start]
-        coeff_rain=np.polyfit(x_selected,rain_selected,degree)
-        p_rain=np.poly1d(coeff_rain)
         values_rain=p_rain(x)
-        #use subplots
-        SelectedData,ax1=plt.subplots()
-        #draw selected data
-        ax1.plot(x_selected,temperature_selected,".",color=temperature_color)
-        ax1.tick_params('y',colors=temperature_color)
-        ax1.set_xlabel("Zeitpunkt")
+        #draw curves
         ax1.plot(x,values_temperature,"--",color="darksalmon")
-        #ax2 with same x-axes for rain
-        ax2=ax1.twinx()
-        ax2.tick_params('y',colors=rain_color)
-        ax2.plot(x_selected,rain_selected,".",color=rain_color)
         ax2.plot(x,values_rain,"--",color="steelblue")
         plt.show()
 
@@ -66,8 +73,8 @@ if __name__ == "__main__":
 	#load data from file
 	data=loadData("data.txt")
 	#extract data
-	start=0
-	end=400
+	start=50
+	end=350
 	temperature=getDataSegment(data,start,end,TX)
 	rain=getDataSegment(data,start,end,RR)
 	plotGraphs(temperature,rain,start,end)
