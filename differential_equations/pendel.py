3bba00be349106623b429678b802f64c140dfa19
Andre Immig <aimmig@students.uni-mainz.de>
2017-06-25 18:56:42 +0200

    Small changes to oszillator, preparations for pendula

--- differential_equations/pendel.py
+++ differential_equations/pendel.py
@@ -5,13 +5,13 @@ import matplotlib.animation as animation
 import random as random
 
 fig,ax=plt.subplots()
-fig.canvas.set_window_title("Harmonic Oszillator")
-ax.set_xlim([-2,2])
-ax.set_ylim([-2,2])
+fig.canvas.set_window_title("Federpendel")
+ax.set_xlim([-1,1])
+ax.set_ylim([-1,1])
 origin=ax.plot(0,0,"x",color="red")
+lot,=ax.plot([0,0],[0,-1],"-",color="red")
 pos,=ax.plot([],[],"o",color="black")
 curve,=ax.plot([],[],"-",color="blue")
-v,=ax.plot([],[],"-",color="green")
 
 #define random start values for position and velocity
 pendel_pos=[[random.uniform(-1,1)],[random.uniform(-1,1)]]
@@ -25,8 +25,6 @@ x_pos=pendel_pos[0]
 y_pos=pendel_pos[1]
 x_vel=pendel_vel[0]
 y_vel=pendel_vel[1]
-v_x=np.array([])
-v_y=np.array([])
 
 #basic version of euler iteration 
 # x'(i+1)=x'(i)+h*x''(i)   mit x''(i)=-c*x(i) für c=D/m
@@ -35,9 +33,6 @@ for i in range(len(t)):
        #calculate new velocity
        x_vel=np.append(x_vel,x_vel[i-1]+dist*(-3*x_pos[i-1]))
        y_vel=np.append(y_vel,y_vel[i-1]+dist*(-3*y_pos[i-1]))
-       #calculate velocity vector for drawing
-       v_x=np.append(v_x,[x_pos[i-1],x_pos[i-1]+x_vel[i-1]])
-       v_y=np.append(v_y,[y_pos[i-1],y_pos[i-1]+y_vel[i-1]])
        #calculate new postion
        x_pos=np.append(x_pos,x_pos[i-1]+dist*x_vel[i-1])
        y_pos=np.append(y_pos,y_pos[i-1]+dist*y_vel[i-1])
@@ -46,21 +41,17 @@ pendel_pos=[x_pos,y_pos]
 pendel_vel=[x_vel,y_vel]
 
 def init ():
-        v.set_data([],[])
         curve.set_data([],[])
         pos.set_data([],[])
         return curve,pos
 
 # step of animation
 def step(i):
-        #draw corresponding velocity vector
-        v.set_data(v_x[2*(i-1):2*i],v_y[2*(i-1):2*i])
         #set all previous points
-        curve.set_data(pendel_pos[0][0:i],pendel_pos[1][0:i])
+        curve.set_data([0,pendel_pos[0][i]],[0,pendel_pos[1][i]])
         #set actual positon
         pos.set_data(pendel_pos[0][i],pendel_pos[1][i])
-        return v,curve,pos
-
+        return curve,pos
 #create animation
 ani=animation.FuncAnimation (fig,step,np.arange(1, len(t)),interval=5,blit=True,init_func =init)
 plt.show ()
