8899b1748ac319a28e7a6990d2183c86ad03a244
Andre Immig <aimmig@students.uni-mainz.de>
2017-07-02 09:31:11 +0200

    Bugfix for pendulum and oszillator

--- differential_equations/oszilattor.py
+++ differential_equations/oszilattor.py
@@ -1,81 +1,84 @@
-from numpy import sin , cos
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import random as random
 
-#define random start values for position and velocity
-pendel_pos=[[random.uniform(-1,1)],[random.uniform(-1,1)]]
-pendel_vel=[[random.uniform(-1,1)],[random.uniform(-1,1)]]
+# define random start values for position and velocity
+pendulum_pos = [[random.uniform(-1, 1)], [random.uniform(-1, 1)]]
+pendulum_vel = [[random.uniform(-1, 1)], [random.uniform(-1, 1)]]
 
-#define grid for euler integration
-dist=0.008
+# define grid for euler integration
+dist = 0.02
 
-fig,ax=plt.subplots()
+fig, ax = plt.subplots()
 fig.canvas.set_window_title("Harmonic Oszillator")
-ax.set_xlim([-2,2])
-ax.set_ylim([-2,2])
-origin=ax.plot(0,0,"x",color="red")
-pos,=ax.plot([],[],"o",color="black")
-curve,=ax.plot([],[],"-",color="blue")
-v,=ax.plot([],[],"-",color="green")
+ax.set_xlim([-2, 2])
+ax.set_ylim([-2, 2])
+origin = ax.plot(0, 0, "x", color="red")
+pos, = ax.plot([], [], "o", color="black")
+curve, = ax.plot([], [], "-", color="blue")
+v, = ax.plot([], [], "-", color="green")
 
-x_pos=pendel_pos[0]
-y_pos=pendel_pos[1]
-x_vel=pendel_vel[0]
-y_vel=pendel_vel[1]
-v_x=np.array([])
-v_y=np.array([])
+x_pos = pendulum_pos[0]
+y_pos = pendulum_pos[1]
+x_vel = pendulum_vel[0]
+y_vel = pendulum_vel[1]
+v_x = np.array([])
+v_y = np.array([])
 
 
 # x'(i+1)=x'(i)+h*x''(i)   mit x''(i)=-c*x(i) für c=D/m
 #  x(i+1)=x(i)+h*x(i)
-#calculate the i-th step of euler iteration
+# calculate the i-th step of euler iteration
 def iteration_step(i):
-       global x_vel,y_vel,x_pos,y_pos
-       #calculate new velocity
-       x_vel_new=x_vel[i-1]+dist*(-3*x_pos[i-1])
-       y_vel_new=y_vel[i-1]+dist*(-3*y_pos[i-1])
-       #calculate velocity vector for drawing
-       v_x_new=[x_pos[i-1],x_pos[i-1]+x_vel[i-1]]
-       v_y_new=[y_pos[i-1],y_pos[i-1]+y_vel[i-1]]
-       #calculate new postion
-       x_pos_new=x_pos[i-1]+dist*x_vel[i-1]
-       y_pos_new=y_pos[i-1]+dist*y_vel[i-1]
-       return [x_vel_new,y_vel_new,v_x_new,v_y_new,x_pos_new,y_pos_new]
+    global x_vel, y_vel, x_pos, y_pos
+    # calculate new velocity
+    x_vel_new = x_vel[i - 1] + dist * (-3 * x_pos[i - 1])
+    y_vel_new = y_vel[i - 1] + dist * (-3 * y_pos[i - 1])
+    # calculate velocity vector for drawing
+    v_x_new = [x_pos[i - 1], x_pos[i - 1] + x_vel[i - 1]]
+    v_y_new = [y_pos[i - 1], y_pos[i - 1] + y_vel[i - 1]]
+    # calculate new position
+    x_pos_new = x_pos[i - 1] + dist * x_vel[i - 1]
+    y_pos_new = y_pos[i - 1] + dist * y_vel[i - 1]
+    return [x_vel_new, y_vel_new, v_x_new, v_y_new, x_pos_new, y_pos_new]
 
-#calls euler-iteration and sets values           
+
+# calls euler-iteration and sets values
 def iterate(i):
-       global x_vel,y_vel,x_pos,y_pos,v_x,v_y
-       global pendel_pos,pendel_vel
-       values=iteration_step(i)
-       x_vel=np.append(x_vel,values[0])
-       y_vel=np.append(y_vel,values[1])
-       v_x=np.append(v_x,values[2])
-       v_y=np.append(v_y,values[3])
-       x_pos=np.append(x_pos,values[4])
-       y_pos=np.append(y_pos,values[5])    
-       pendel_pos=[x_pos,y_pos]
-       pendel_vel=[x_vel,y_vel]
+    global x_vel, y_vel, x_pos, y_pos, v_x, v_y
+    global pendulum_pos, pendulum_vel
+    values = iteration_step(i)
+    x_vel = np.append(x_vel, values[0])
+    y_vel = np.append(y_vel, values[1])
+    v_x = np.append(v_x, values[2])
+    v_y = np.append(v_y, values[3])
+    x_pos = np.append(x_pos, values[4])
+    y_pos = np.append(y_pos, values[5])
+    pendulum_pos = [x_pos, y_pos]
+    pendulum_vel = [x_vel, y_vel]
 
-def init ():
-        v.set_data([],[])
-        curve.set_data([],[])
-        pos.set_data([],[])
-        return curve,pos
+
+def init():
+    v.set_data([], [])
+    curve.set_data([], [])
+    pos.set_data([], [])
+    return curve, pos
+
 
 # step of animation
 def step(i):
-        global pendel_pos,pendel_vel,v_x,v_y
-        #set all previous points
-        curve.set_data(pendel_pos[0][0:i-1],pendel_pos[1][0:i-1])
-        #set actual positon
-        pos.set_data(pendel_pos[0][i-1],pendel_pos[1][i-1])
-        iterate(i)
-        #draw corresponding velocity vector
-        v.set_data(v_x[2*(i-1):2*i],v_y[2*(i-1):2*i])
-        return v,curve,pos
+    global pendulum_pos, pendulum_vel, v_x, v_y
+    # set all previous points
+    curve.set_data(pendulum_pos[0][0:i - 1], pendulum_pos[1][0:i - 1])
+    # set actual position
+    pos.set_data(pendulum_pos[0][i - 1], pendulum_pos[1][i - 1])
+    iterate(i)
+    # draw corresponding velocity vector
+    v.set_data(v_x[2 * (i - 1):2 * i], v_y[2 * (i - 1):2 * i])
+    return v, curve, pos
 
-#create animation
-ani=animation.FuncAnimation (fig,step,np.arange(1,100000),interval=10,blit=True,init_func =init)
-plt.show ()
+
+# create animation
+ani = animation.FuncAnimation(fig, step, np.arange(0, 100000), interval=15, blit=True, init_func=init)
+plt.show()
--- differential_equations/pendel.py
+++ differential_equations/pendel.py
@@ -1,65 +1,56 @@
-from numpy import sin,cos
-from math import sqrt
+from numpy import sin, cos
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import random as random
 
-fig,ax=plt.subplots()
+fig, ax = plt.subplots()
 fig.canvas.set_window_title("Federpendel")
-ax.set_xlim([-2,2])
-ax.set_ylim([-2,2])
-origin=ax.plot(0,0,"x",color="red")
-lot,=ax.plot([0,0],[0,-2],"-",color="red")
-pos,=ax.plot([],[],"o",color="black")
-curve,=ax.plot([],[],"-",color="blue")
+ax.set_xlim([-2, 2])
+ax.set_ylim([-2, 2])
+origin = ax.plot(0, 0, "x", color="red")
+lot, = ax.plot([0, 0], [0, -2], "-", color="red")
+pos, = ax.plot([], [], "o", color="black")
+curve, = ax.plot([], [], "-", color="blue")
 
-start_x=random.uniform(0,1)
-start_y=random.uniform(-1,0)
-start_vel_x=random.uniform(0,10)
-start_vel_y=random.uniform(-10,0)
-#define random start values for position and velocity
-pendel_pos=[[start_x],[start_y]]
-pendel_vel=[[3],[-3]]
-l=sqrt(start_x**2+start_y**2)
-factor=-9.81/l
-#define grid for euler integration
-t=np.arange(0,10000,0.03)
-dist=t[1]-t[0]
-c=1
+# norm length of pendulum to 1
+factor = -9.81
+# define grid for euler integration
+t = np.arange(0, 10000, 0.01)
+dist = t[1] - t[0]
 
-x_pos=pendel_pos[0]
-y_pos=pendel_pos[1]
-x_vel=pendel_vel[0]
-y_vel=pendel_vel[1]
+alpha_vel = [random.uniform(-3, 3)]
+alpha = [random.uniform(0, 180)]
+pendulum_pos = [sin(alpha), -cos(alpha)]
 
-#basic version of euler iteration 
+
+# basic version of euler iteration
 # a'(i+1)=a'(i)+h*a''(i)   mit a''(i)=-g/l*sin(a(i))
 #  a(i+1)=a(i)+h*a'(i)                    
-for i in range(len(t)):
-        x_vel.append(c*(x_vel[i-1]+dist*factor*sin(x_pos[i-1])))
-        y_vel.append(c*(y_vel[i-1]+dist*factor*sin(y_pos[i-1])))
-        x_pos.append(x_pos[i-1]+dist*x_vel[i-1])
-        y_pos.append(y_pos[i-1]+dist*y_vel[i-1])
-        #Dämpfungs-Faktor sinkt sehr langsam (proportional zu -sqrt(i)/c)
-        c=(1-4*sqrt(i)/8000)   
+def iteration_step(i: int):
+    global pendulum_pos
+    alpha_vel.append(alpha_vel[i - 1] + dist * factor * sin(alpha[i - 1]))
+    alpha.append(alpha[i - 1] + dist * alpha_vel[i - 1])
+    # reconstruct positions from alpha
+    pendulum_pos = [sin(alpha), -cos(alpha)]
 
-#reconstruct positons from alpha
-pendel_pos=[l*sin(x_pos),-l*cos(y_pos)]
-pendel_vel=[x_vel,y_vel]
 
-def init ():
-        curve.set_data([],[])
-        pos.set_data([],[])
-        return curve,pos
+def init():
+    curve.set_data([], [])
+    pos.set_data([], [])
+    return curve, pos
+
 
 # step of animation
-def step(i):
-        #construct line with 
-        curve.set_data([0,pendel_pos[0][i]],[0,pendel_pos[1][i]])
-        #set actual positon
-        pos.set_data(pendel_pos[0][i],pendel_pos[1][i])
-        return curve,pos
-#create animation
-ani=animation.FuncAnimation (fig,step,np.arange(1, len(t)),interval=40,blit=True,init_func =init)
-plt.show ()
+def step(j: int):
+    # construct line with
+    curve.set_data([0, pendulum_pos[0][j]], [0, pendulum_pos[1][j]])
+    # set actual position
+    pos.set_data(pendulum_pos[0][j], pendulum_pos[1][j])
+    iteration_step(j)
+    return curve, pos
+
+
+# create animation
+ani = animation.FuncAnimation(fig, step, np.arange(0, len(t)), interval=50, blit=True, init_func=init)
+plt.show()
