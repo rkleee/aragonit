110363f1ccc68b314b7d439ec9efafabe75bcd6e
Christian Hecktor <checktor@users.noreply.github.com>
2017-07-04 01:12:16 +0200

    Create separate layers for sky and land mass
    The sky layer is the base with the land layer on top of
    it. Therefore removing the land mass will reveal the sky.

--- worms/Layers.py
+++ worms/Layers.py
@@ -1,35 +1,64 @@
 """Module handling the different layers of the map."""
 from math import pi, sin, sqrt
-from random import randrange
+from random import uniform
 
 import numpy as np
 import PyQt5.QtCore as core
 import PyQt5.QtGui as gui
 
 
-class MapLayer(gui.QImage):
-    """QImage as a base layer containing the landscape of the map."""
+class LandLayer(gui.QImage):
+    """Layer containing the land mass of the map."""
 
-    def computeLandscape(self, x_coordinate):
+    def _computeLandscapeParameters(self):
+        """Set all constant factors for landscape creation function."""
+        self.landscape_compression_factor = uniform(-1, 1)
+        self.landscape_offset = uniform(0, 2 * pi)
+        self.landscape_parameters = np.linspace(0.001, 0.05, 200)
+
+    def _getLandscapeHeight(self, x_coordinate):
         """
         Return the height of the land to a given x value.
 
-        Uses randomly shifted and stretched / compressed sinus functions
-        to compute the landscape.
+        Uses the sum of different randomly shifted and stretched
+        or compressed sinus functions to compute the landscape.
         """
-        W = np.linspace(0.001, 0.05, 20)
         y_coordinate = 0
-        for w in W:
-            intermediate_result = (
-                1 / sqrt(w)) * sin(w * x_coordinate + randrange(0, 2 * pi)) * randrange(-1, 1)
+        for param in self.landscape_parameters:
+            weight_factor = 1 / sqrt(param)
+            intermediate_result = weight_factor * \
+                sin(param * x_coordinate + self.landscape_offset) * \
+                self.landscape_compression_factor
             y_coordinate += intermediate_result
         return y_coordinate
 
-    def __init__(self, width, height):
+    def _createLandscape(self):
+        vertical_middle = len(self.color_data[0]) // 2
+        for x in range(len(self.color_data)):
+            border = self._getLandscapeHeight(x) + vertical_middle
+            for y in range(len(self.color_data[0])):
+                if y >= border:
+                    self.color_data[y][x] = [0, 150, 0, 255]
+
+    def __init__(self, width, height, color_format):
         """Initialize the landscape."""
-        color_data = np.zeros([width, height, 4], dtype=np.uint8)
-        color_format = gui.QImage.Format_RGB32
-        super(MapLayer, self).__init__(color_data, width, height, color_format)
-        painter = gui.QPainter(self)
-        painter.setPen(gui.QPen(core.Qt.blue, 2))
-        painter.drawLine(50, 50, 50, 100)
+        self.color_data = np.zeros([width, height, 4], dtype=np.uint8)
+        self._computeLandscapeParameters()
+        self._createLandscape()
+        super(LandLayer, self).__init__(
+            self.color_data, width, height, color_format)
+        # painter = gui.QPainter(self)
+        # painter.setPen(gui.QPen(core.Qt.blue, 2))
+        # painter.drawLine(50, 50, 50, 100)7
+
+
+class SkyLayer(gui.QImage):
+    """Layer containing the sky."""
+
+    def __init__(self, width, height, color_format):
+        self.color_data = np.zeros([width, height, 4], dtype=np.uint8)
+        for i in range(width):
+            for j in range(height):
+                self.color_data[j][i] = [0, 0, 200, 255]
+        super(SkyLayer, self).__init__(
+            self.color_data, width, height, color_format)
--- worms/Main.py
+++ worms/Main.py
@@ -6,12 +6,23 @@ import PyQt5.QtWidgets as widget
 
 import Layers
 
+
+def drawMap(width, height):
+    color_format = gui.QImage.Format_RGBA8888
+    sky_layer = Layers.SkyLayer(width, height, color_format)
+    land_layer = Layers.LandLayer(width, height, color_format)
+    painter = gui.QPainter(sky_layer)
+    painter.drawImage(0, 0, land_layer)
+    painter.end()
+    return sky_layer
+
+
 if __name__ == "__main__":
     app = widget.QApplication(sys.argv)
     width = 500
     height = 500
-    world_layer = Layers.MapLayer(width, height)
+    world = drawMap(width, height)
     display = widget.QLabel()
-    display.setPixmap(gui.QPixmap.fromImage(world_layer))
+    display.setPixmap(gui.QPixmap.fromImage(world))
     display.show()
     sys.exit(app.exec_())
