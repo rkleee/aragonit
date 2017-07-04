1e97883bdc1b645db99849922af076e8c4f6445c
Christian Hecktor <checktor@users.noreply.github.com>
2017-07-05 00:19:29 +0200

    Fix bug concerning strange positioning of pixels on the QImage

--- worms/Layers.py
+++ worms/Layers.py
@@ -7,13 +7,13 @@ import PyQt5.QtCore as core
 import PyQt5.QtGui as gui
 
 
-class LandLayer(gui.QImage):
+class LandscapeLayer(gui.QImage):
     """Layer containing the land mass of the map."""
 
     def _computeLandscapeParameters(self):
-        """Set all constant factors for landscape creation function."""
-        self.landscape_compression_factor = uniform(-1, 1)
-        self.landscape_offset = uniform(0, 2 * pi)
+        """Set constant factors for landscape creation function."""
+        self.landscape_compression_factor = uniform(-1.0, 1.0)
+        self.landscape_offset = uniform(0.0, 2.0 * pi)
         self.landscape_parameters = np.linspace(0.001, 0.05, 200)
 
     def _getLandscapeHeight(self, x_coordinate):
@@ -33,32 +33,34 @@ class LandLayer(gui.QImage):
         return y_coordinate
 
     def _createLandscape(self):
-        vertical_middle = len(self.color_data[0]) // 2
-        for x in range(len(self.color_data)):
+        """Create a random landscape."""
+        vertical_middle = len(self.color_data) / 2
+        for x in range(len(self.color_data[0])):
             border = self._getLandscapeHeight(x) + vertical_middle
-            for y in range(len(self.color_data[0])):
+            for y in range(len(self.color_data)):
                 if y >= border:
-                    self.color_data[y][x] = [0, 150, 0, 255]
+                    self.color_data[y, x, :] = self.land_color
 
     def __init__(self, width, height, color_format):
         """Initialize the landscape."""
-        self.color_data = np.zeros([width, height, 4], dtype=np.uint8)
+        # set the color for the land
+        self.land_color = [139, 69, 19, 255]
+        # create a 3 dimensional matrix representing the image's color data
+        self.color_data = np.zeros([height, width, 4], dtype=np.uint8)
+        # initialize the landscape
         self._computeLandscapeParameters()
         self._createLandscape()
-        super(LandLayer, self).__init__(
-            self.color_data, width, height, color_format)
+        super().__init__(self.color_data, width, height, color_format)
         # painter = gui.QPainter(self)
         # painter.setPen(gui.QPen(core.Qt.blue, 2))
-        # painter.drawLine(50, 50, 50, 100)7
+        # painter.drawLine(50, 50, 50, 100)
 
 
-class SkyLayer(gui.QImage):
-    """Layer containing the sky."""
+class BackgroundLayer(gui.QImage):
+    """Layer containing the background of the map."""
 
     def __init__(self, width, height, color_format):
-        self.color_data = np.zeros([width, height, 4], dtype=np.uint8)
-        for i in range(width):
-            for j in range(height):
-                self.color_data[j][i] = [0, 0, 200, 255]
-        super(SkyLayer, self).__init__(
-            self.color_data, width, height, color_format)
+        """Initialize the background."""
+        super().__init__(width, height, color_format)
+        # use a simple blue sky as background
+        self.fill(core.Qt.blue)
--- worms/Main.py
+++ worms/Main.py
@@ -7,22 +7,24 @@ import PyQt5.QtWidgets as widget
 import Layers
 
 
-def drawMap(width, height):
+def createMapImage(width, height):
+    """Create a QImage containing all the different layers of the map."""
     color_format = gui.QImage.Format_RGBA8888
-    sky_layer = Layers.SkyLayer(width, height, color_format)
-    land_layer = Layers.LandLayer(width, height, color_format)
-    painter = gui.QPainter(sky_layer)
-    painter.drawImage(0, 0, land_layer)
+    background_layer = Layers.BackgroundLayer(width, height, color_format)
+    landscape_layer = Layers.LandscapeLayer(width, height, color_format)
+    painter = gui.QPainter(background_layer)
+    painter.drawImage(0, 0, landscape_layer)
     painter.end()
-    return sky_layer
+    return background_layer
 
 
 if __name__ == "__main__":
     app = widget.QApplication(sys.argv)
-    width = 500
-    height = 500
-    world = drawMap(width, height)
+    width = 2000
+    height = 1000
+    map_image = createMapImage(width, height)
     display = widget.QLabel()
-    display.setPixmap(gui.QPixmap.fromImage(world))
+    display.setPixmap(gui.QPixmap.fromImage(map_image))
+    display.setScaledContents(True)
     display.show()
     sys.exit(app.exec_())
